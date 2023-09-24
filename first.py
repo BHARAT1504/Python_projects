a3540973bd4ef70f07374af417a3437a64436bea
BHARAT1504 <76224177+BHARAT1504@users.noreply.github.com>
2023-09-24 18:02:18 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,39 @@
+# # # # # print("hello world ")
+# # # # # first="tonny";
+# # # # # last="stark";
+# # # # # print(first + last)
+# # # # # name=input("name: ")
+# # # # # print(name)
+
+# # # # # a=input("enter num1 ")
+# # # # # b=input("enter num2 ")
+
+# # # # # sum=int(a)+int(b)
+# # # # # print("sum is "+str(sum))imj
+# # # # #name="bharat kumar "
+# # # # #print(name.find('k'))
+# # # # #print(name.replace("kumar"),"choudhary ")
+# # # # i=1
+# # # # while i<=5:
+# # # #     print(i*"*")
+# # # #     i=i+1
+
+# # # # j=5    
+# # # # while j>=0:
+# # # #     print(j*"*")
+# # # #     j=j-1    
+
+# # # std=["ram","shyam","krishna","ramu"]
+
+# # # for std in std:
+# # #     if std=="ramu":
+# # #         break
+# # #     print(std)
+
+# # marks = (95, 96, 88, 86, 96)
+# # print(marks.count(96))
+# import math
+# print(dir(math)
+
+from math import *
+print(sqrt(36))
